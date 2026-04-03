578636552cfe7b3b15fb19986dbfac3f8533d307
sofiasarak <sofiasarak30@gmail.com>
2026-04-03 15:17:11 -0700

    Initial deployment code and logs

--- two-file-app/server.R
+++ two-file-app/server.R
@@ -1,2 +1,63 @@
 # ---- server ----
-server <- function(input, output){}
+
+# server is serving the user what they are asking for through the UI
+
+server <- function(input, output){
+  
+  
+  # filtered trout data ----
+  trout_filtered_df <- reactive({ 
+    
+    clean_trout |> 
+      filter(channel_type %in% c(input$channel_type_input)) %>% 
+      filter(section %in% c(input$section_input))
+  })
+  
+  # render trout scatterplot ----
+  output$trout_scatterplot_output <- renderPlot({
+    
+    ggplot(trout_filtered_df(), aes(x = length_mm, y = weight_g, 
+                                  color = channel_type, shape = channel_type)) +
+      geom_point(alpha = 0.7, size = 5) +
+      scale_color_manual(values = c("cascade" = "#2E2585", 
+                                    "riffle" = "#337538", 
+                                    "isolated pool" = "#DCCD7D", 
+                                    "pool" = "#5DA899", 
+                                    "rapid" = "#C16A77", 
+                                    "step (small falls)" = "#9F4A96", 
+                                    "side channel" = "#94CBEC")) +
+      scale_shape_manual(values = c("cascade" = 15, 
+                                    "riffle" = 17, 
+                                    "isolated pool" = 19, 
+                                    "pool" = 18, 
+                                    "rapid" = 8, 
+                                    "step (small falls)" = 23, 
+                                    "side channel" = 25)) +
+      labs(x = "Trout Length (mm)", 
+           y = "Trout Weight (g)", 
+           color = "Channel Type", 
+           shape = "Channel Type") +
+      myCustomTheme()
+    
+  })
+    
+# filter penguin data 
+
+island_df <- reactive({   
+   
+  penguins |> 
+      filter(island %in% c(input$penguin_island_input))
+})
+    
+# render penguin histogram
+    output$penguin_flipper_length_plot <- renderPlot({
+      
+      ggplot(na.omit(island_df()), aes(x = flipper_length_mm, fill = species)) +
+        geom_histogram(alpha = 0.6, position = "identity", bins = input$histogram_bin_slider_input) +
+        scale_fill_manual(values = c("Adelie" = "#FEA346", "Chinstrap" = "#B251F1", "Gentoo" = "#4BA4A4")) +
+        labs(x = "Flipper length (mm)", y = "Frequency",
+             fill = "Penguin species") +
+        myCustomTheme()
+    })
+    
+}
--- two-file-app/ui.R
+++ two-file-app/ui.R
@@ -1,2 +1,120 @@
 # ---- user interface ---- anything that I want to end up on the website needs to live in ui
-ui <- fluidPage()
+ui <- navbarPage( # similar to fluidpage but adds a navbar at the top
+  
+  # title
+  title = "LTER Animal Data Explorer",
+  
+  # (Page 1) intro tabPanel
+  tabPanel(title = "About this App",
+           
+           # intro text fluidRow ----
+           fluidRow(
+             
+            column(1), # add empty columns as buffers for spacing 
+            column(10, includeMarkdown("text/about.md")),
+            column(1)
+             
+           ), # END intro text fluidRow
+           
+           hr(), # adds gray horizontal line as visual break
+           
+           includeMarkdown("text/footer.md") # don't need to define explicit rows because we want footer to run across entire page
+           
+  ), # END (Page 1) intro tabPanel
+  
+  # (Page 2) data tabPanel ----
+  
+## START TROUT TAB
+
+  tabPanel(title = "Explore the Data",
+           
+           # tabsetPanel to contain tabs for data viz ----
+           tabsetPanel(
+             
+             # trout tabPanel ----
+             tabPanel(
+               
+               title = "Trout",
+               
+               # trout sidebarLayout ----
+               sidebarLayout(
+                 
+                 # trout sidebarPanel ---
+                 sidebarPanel(
+                   
+                   # channel type pickerinput ---
+                   pickerInput(inputId = "channel_type_input",
+                               label = "Select channal type(s):",
+                               choices = unique(clean_trout$channel_type),
+                               selected = c("cascade", "pool"),
+                               multiple = TRUE, # allows for selection of multiple options at a time
+                               options = pickerOptions(actionsBox = TRUE) # gives the option of selecting all or deselecting all
+                                 ), # END channel type picker input
+                   
+                   # section checkboxGroupButtons ----
+                   checkboxGroupButtons(inputId = "section_input",
+                                        label = "Select a sampling section(s):",
+                                        choices = c("clear cut forest", "old growth forest"),
+                                        selected = c("clear cut forest", "old growth forest"),
+                                        justified = TRUE, # stretch to size of sidebar panel
+                                        checkIcon = list(yes = icon("check", lib = "font-awesome"), # icon for when button is clicked
+                                                         no = icon("xmark", lib = "font-awesome")))
+              
+                   
+                 ), # END trout sidebarPanel
+                 
+                 # trout mainPanel ----
+                 mainPanel(
+                   
+                   plotOutput(outputId = "trout_scatterplot_output")
+                   
+                 ) # END trouth mainPanel
+                 
+               ) # END trout sidebar layout
+               
+             ), # END trout tabPanel
+             
+             
+## START PENGUINS TAB
+             
+             tabPanel(
+               
+               title = "Penguins",
+               
+               # penguins sidebarLayout ----
+               sidebarLayout(
+                 
+                 # penguins sidebarPanel ---
+                 sidebarPanel(
+                   
+                   # penguins island selection picker
+                   pickerInput(input = "penguin_island_input",
+                               label = "Select island(s):",
+                               choices = unique(penguins$island),
+                               selected = c("Biscoe", "Dream"),
+                               multiple = TRUE,
+                               options = pickerOptions(actionsBox = TRUE) # gives option to select all or none
+                    ), # END island picker
+                   
+                   sliderInput(inputId = "histogram_bin_slider_input",
+                               label = "Choose the number of bins:",
+                               min = 1, max = 100,
+                               value = 25)
+                   
+                 ), # END penguin sidebarPanel
+                 
+                 # penguins mainPanel ----
+                 mainPanel(
+                   
+                   plotOutput(outputId = "penguin_flipper_length_plot")
+                   
+                 ) # END penguin mainPanel
+                 
+               ) # END penguin sidebar layout
+             ) # END penguins tabPanel
+             
+           ) # END tabsetPanel
+           
+  ), # END (Page 2) data tabPanel
+  
+) # END navbarPage
